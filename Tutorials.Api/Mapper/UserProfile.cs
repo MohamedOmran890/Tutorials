bade29ef5ff575416fb5fd3f2dc27d08c0a394cb
alyaa999 <alyaamamoon999@gmail.com>
2023-07-30 07:20:42 +0300

    add auto mapper configs and Register action

---
+++ Tutorials.Api/Mapper/UserProfile.cs
@@ -0,0 +1,18 @@
+
+using AutoMapper;
+using Tutorials.Api.DTO;
+using Tutorials.Data.Entities;
+
+namespace Tutorials.Api.Mapper 
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            // source to destiation 
+            CreateMap<RegisterDTO, User>().ReverseMap();
+
+        }
+    }
+
+}
