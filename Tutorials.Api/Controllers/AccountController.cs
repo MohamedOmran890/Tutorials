45c8d47bbd25d386b63e0388ea486d04230c48a5
alyaa999 <alyaamamoon999@gmail.com>
2023-07-31 07:35:53 +0300

    WIP: Add login action and jwt config

--- Tutorials.Api/Controllers/AccountController.cs
+++ Tutorials.Api/Controllers/AccountController.cs
@@ -1,26 +1,46 @@
 
+using System;
+using System.Data.Common;
+
+using System.Security.Claims;
 using System.Reflection;
 using Tutorials.Api.DTO;
 using Microsoft.AspNetCore.Mvc;
-using Tutorials.Data.Context ;
-using Tutorials.Data.Entities ;
+using Tutorials.Data.Context;
+using Tutorials.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
-using AutoMapper ;
+using AutoMapper;
+using System.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+using Microsoft.IdentityModel.Tokens;
+
+
+
 namespace Tutorials.Api.Controllers
 {
 
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
+
     public class AccountController : ControllerBase
     {
         private readonly UserManager<User> _userManager;
         private readonly IMapper _mapper;
-
-        public AccountController(UserManager<User> userManager  , IMapper mapper)
+        private readonly IConfiguration _configuration;
+    
+        public AccountController(UserManager<User> userManager, IMapper mapper, IConfiguration configuration)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _configuration = configuration;
         }
         [HttpPost("Register")]
         public async Task<IActionResult> Register(RegisterDTO UserDto)
@@ -28,21 +48,67 @@ namespace Tutorials.Api.Controllers
             if (ModelState.IsValid)
             {
                 // using auto mapper 
-            
-            var user   =  _mapper.Map<User>(UserDto);
-            IdentityResult result =  await  _userManager.CreateAsync(user , UserDto.Password);
-            if (result.Succeeded)
-            {
-                return  Ok ("Account Add Success");
-            }
-            else return BadRequest(result);
+
+                var user = _mapper.Map<User>(UserDto);
+                IdentityResult result = await _userManager.CreateAsync(user, UserDto.Password);
+                if (result.Succeeded)
+                {
+                    return Ok("Account Add Success");
+                }
+                else return BadRequest(result);
 
             }
             return BadRequest(ModelState);
 
+        }
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login(LoginDTO UserDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(UserDto.UserName);
+                if (user != null)
+                {
+                    bool found = await _userManager.CheckPasswordAsync(user, UserDto.Password);
+                    if (found)
+                    {
+                        var claims = new[] {
+                        new Claim(JwtRegisteredClaimNames.Name, UserDto.UserName),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) ,
+                        new Claim(JwtRegisteredClaimNames.NameId, user.Id) };
+
+                        var roles = await _userManager.GetRolesAsync(user);
+                        foreach (var role in roles)
+                        {
+                            Claim claim = new Claim(JwtRegisteredClaimNames.Name, UserDto.UserName);
+                            claims.Append(claim);
+                        }
+                        SecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
 
 
+                        SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
+                        JwtSecurityToken Token = new JwtSecurityToken(
+                            issuer: _configuration["Jwt:Issuer"],
+                            audience: _configuration["Jwt:Audience"],
+                            claims: claims,
+                            expires: DateTime.Now.AddMinutes(120),
+                            signingCredentials: signingCredentials
+                        );
+
+                        return Ok(new
+                        {
+                            token = new JwtSecurityTokenHandler().WriteToken(Token),
+                            expiration = Token.ValidTo
+                        });
+
+
+                    }
+                }
+                return Unauthorized();
+
+            }
+            return Unauthorized();
 
         }
 
--- Tutorials.Api/Program.cs
+++ Tutorials.Api/Program.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Net.NetworkInformation;
+using System.Buffers;
+using System.Runtime.InteropServices;
+using System.Reflection.PortableExecutable;
 using Microsoft.EntityFrameworkCore;
 using Tutorial.Infstructures.Interfaces;
 using Tutorial.Infstructures.Repository;
@@ -7,16 +12,29 @@ using AutoMapper;
 using Tutorials.Data.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Tutorials.Api.Mapper;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Any;
 
 
 namespace Tutorials.Api
 {
     public class Program
     {
+        private static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
+
             var builder = WebApplication.CreateBuilder(args);
+           Configuration = builder.Configuration;
 
             // Add services to the container.
             builder.Services.AddControllers();
@@ -29,7 +47,61 @@ namespace Tutorials.Api
             builder.Services.AddTransient<ITeacherRepository, TeacherRepository>();
             builder.Services.AddAutoMapper(typeof(UserProfile));
             builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<TutorialDbContext>();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }
 
+
+
+            ).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+
+                };
+
+            });
+            builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+    
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -40,8 +112,9 @@ namespace Tutorials.Api
             }
 
             app.UseHttpsRedirection();
-
+            app.UseAuthentication();
             app.UseAuthorization();
+
 
 
             app.MapControllers();
